f271d56cf0e5db9d369c406201cbacf47150f300
Chiara Chiappini <cchiappini@cchiappini-macbookpro.roam.corp.google.com>
2015-11-06 16:36:55 +0000

    robolectriv test using 3.0

--- londonWeather/src/test/java/com.cchiappini.londonweather/MainActivityTest.java
+++ londonWeather/src/test/java/com.cchiappini.londonweather/MainActivityTest.java
@@ -6,12 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import static junit.framework.Assert.assertNotNull;
 
-@Config(constants = BuildConfig.class, sdk = 22)
-@RunWith(MyRobolectricGradleTestRunner.class)
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class MainActivityTest {
 
     private Activity mainActivity;
